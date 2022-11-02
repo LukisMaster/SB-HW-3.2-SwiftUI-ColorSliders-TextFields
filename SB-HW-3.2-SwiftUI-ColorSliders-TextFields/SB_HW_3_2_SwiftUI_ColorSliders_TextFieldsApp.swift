7ed9c6d28f09561ee8adc8dec27918d78b1f7a5c
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-11-02 16:23:00 +0300

    Initial Commit

---
+++ SB-HW-3.2-SwiftUI-ColorSliders-TextFields/SB_HW_3_2_SwiftUI_ColorSliders_TextFieldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SB_HW_3_2_SwiftUI_ColorSliders_TextFieldsApp.swift
+//  SB-HW-3.2-SwiftUI-ColorSliders-TextFields
+//
+//  Created by Sergey Nestroyniy on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SB_HW_3_2_SwiftUI_ColorSliders_TextFieldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
